936b54b6f69c2a6578e7eeb9ac65fdadfb5b30fa
Alice Jost <alice.jost@outlook.de>
2022-03-11 19:44:05 +0100

    report bug mail link button

---
+++ src/components/ReportBug.tsx
@@ -0,0 +1,16 @@
+export default function ReportBug() {
+    return (
+      <>
+        <div className="right-side">
+          <a
+            className="btn"
+            href="mailto:language4ukraine@gmail.com ?subject=BUG REPORT"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Report bug
+          </a>
+        </div>
+      </>
+    );
+}
