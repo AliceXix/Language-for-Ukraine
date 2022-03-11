16469edc45c68fb3016d701f9241e9a50a333dbc
Alice Jost <alice.jost@outlook.de>
2022-03-11 19:06:33 +0100

    create banner

---
+++ src/components/Banner.tsx
@@ -0,0 +1,14 @@
+export default function Banner() {
+    return (
+      <>
+        <a
+          id="banner"
+          href="https://savelife.in.ua/en/donate/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          💙 Donate if you can 💛
+        </a>
+      </>
+    );
+}
